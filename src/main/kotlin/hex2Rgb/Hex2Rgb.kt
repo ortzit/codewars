48fb73dafc16bbb932766487d9e98bf310effe87
ortzi <ortzi.torices@gmail.com>
2022-10-14 23:16:53 +0200

    adds refactor

--- src/main/kotlin/hex2Rgb/Hex2Rgb.kt
+++
@@ -1,9 +0,0 @@
-package hex2Rgb
-
-// you have preloaded
-// data class RGB(val r: Int, val g: Int, val b: Int)
-fun hexStringToRGB(hexString: String): RGB {
-    TODO()
-}
-
-data class RGB(val r: Int, val g: Int, val b: Int)
--- src/main/kotlin/rgb2hex/Rgb2Hex.kt
+++ src/main/kotlin/rgb2hex/Rgb2Hex.kt
@@ -3,9 +3,11 @@ package rgb2hex
 private const val MIN = 0
 private const val MAX = 255
 private const val BASE = 16
-private val VALUES = arrayOf("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
+private val VALUES = IntRange(0, 15).map { it.hexAsciiChar() }
 
 fun rgb(r: Int, g: Int, b: Int): String = "${r.toHex()}${g.toHex()}${b.toHex()}"
+
+private fun Int.hexAsciiChar() = (this.takeIf { it < 10 }?.let { this + 48 } ?: (this + 55)).toChar()
 
 private fun Int.toHex(): String = this.coerceAtLeast(MIN).coerceAtMost(MAX).let { normalizedValue ->
     "${VALUES[normalizedValue / BASE]}${VALUES[normalizedValue % BASE]}"
