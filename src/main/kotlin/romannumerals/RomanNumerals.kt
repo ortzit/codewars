84e937d71f64796ce0c6075c2e132c1399e81a30
ortzi <ortzi.torices@gmail.com>
2022-10-14 15:40:24 +0200

    Basic cases implemented

--- src/main/kotlin/romannumerals/RomanNumerals.kt
+++ src/main/kotlin/romannumerals/RomanNumerals.kt
@@ -1,5 +1,21 @@
 package romannumerals
 
 fun decode(str: String): Int {
-    TODO()
+    var decoded = 0
+    str.forEach { character ->
+        decoded += characterDecode(character)
+    }
+    return decoded
 }
+
+private fun characterDecode(c: Char): Int =
+    when(c) {
+        'I' -> 1
+        'V' -> 5
+        'X' -> 10
+        'L' -> 50
+        'C' -> 100
+        'D' -> 500
+        'M' -> 1000
+        else -> throw RuntimeException("Invalid value [$c]")
+    }
