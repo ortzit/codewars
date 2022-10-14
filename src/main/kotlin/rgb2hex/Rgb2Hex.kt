3c0a997b0b43749aa48cd3f14122b9c0c8e99865
ortzi <ortzi.torices@gmail.com>
2022-10-14 23:22:49 +0200

    adds rgb optimized version

--- src/main/kotlin/rgb2hex/Rgb2Hex.kt
+++ src/main/kotlin/rgb2hex/Rgb2Hex.kt
@@ -7,6 +7,9 @@ private val VALUES = IntRange(0, 15).map { it.hexAsciiChar() }
 
 fun rgb(r: Int, g: Int, b: Int): String = "${r.toHex()}${g.toHex()}${b.toHex()}"
 
+fun rgbOptimized(r: Int, g: Int, b: Int): String =
+    String.format("%02X%02X%02X", r.coerceIn(0..255), g.coerceIn(0..255), b.coerceIn(0..255))
+
 private fun Int.hexAsciiChar() = (this.takeIf { it < 10 }?.let { this + 48 } ?: (this + 55)).toChar()
 
 private fun Int.toHex(): String = this.coerceAtLeast(MIN).coerceAtMost(MAX).let { normalizedValue ->
--- src/test/kotlin/rgb2hex/Rgb2HexKtTest.kt
+++ src/test/kotlin/rgb2hex/Rgb2HexKtTest.kt
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Test
 
 internal class Rgb2HexKtTest {
     @Test
-    fun testFixed() {
+    fun testFixed_rgb() {
         assertEquals("000000", rgb(0, 0, 0))
         assertEquals("000000", rgb(0, 0, -20))
         assertEquals("FFFFFF", rgb(300,255,255))
         assertEquals("ADFF2F", rgb(173,255,47))
         assertEquals("9400D3", rgb(148, 0, 211))
+    }
+
+    @Test
+    fun testFixed_rgbOptimized() {
+        assertEquals("000000", rgbOptimized(0, 0, 0))
+        assertEquals("000000", rgbOptimized(0, 0, -20))
+        assertEquals("FFFFFF", rgbOptimized(300,255,255))
+        assertEquals("ADFF2F", rgbOptimized(173,255,47))
+        assertEquals("9400D3", rgbOptimized(148, 0, 211))
     }
 }
