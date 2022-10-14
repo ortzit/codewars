c351e3cf4c264368d73552b4436221b4e0eeda9c
ortzi <ortzi.torices@gmail.com>
2022-10-14 22:51:01 +0200

    refactors code

--- src/main/kotlin/rgb2hex/Rgb2Hex.kt
+++ src/main/kotlin/rgb2hex/Rgb2Hex.kt
@@ -2,10 +2,11 @@ package rgb2hex
 
 private const val MIN = 0
 private const val MAX = 255
+private const val BASE = 16
 private val VALUES = arrayOf("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
 
 fun rgb(r: Int, g: Int, b: Int): String = "${r.toHex()}${g.toHex()}${b.toHex()}"
 
-private fun Int.toHex(): String = this.coerceAtLeast(MIN).coerceAtMost(MAX).let { rangedValue ->
-    "${VALUES[rangedValue / 16]}${VALUES[rangedValue % 16]}"
+private fun Int.toHex(): String = this.coerceAtLeast(MIN).coerceAtMost(MAX).let { normalizedValue ->
+    "${VALUES[normalizedValue / BASE]}${VALUES[normalizedValue % BASE]}"
 }
